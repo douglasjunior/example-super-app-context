52d953855a26ce8b83eb0a595a94c494e9840a35
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2022-07-05 14:31:50 -0300

    Desativa o crashlyticas caso o SDK não esteja configurado.

--- lib/utils/log/CrashlyticsLogProvider.ts
+++ lib/utils/log/CrashlyticsLogProvider.ts
@@ -1,27 +1,32 @@
-import crashlytics, {
-  FirebaseCrashlyticsTypes,
-} from '@react-native-firebase/crashlytics';
+import firebaseApp from '@react-native-firebase/app';
 import {SetupPropsType} from '../../types';
 import AbstractLogProvider from './AbstractLogProvider';
 
 class CrashlyticsLogProvider extends AbstractLogProvider {
-  private instace: FirebaseCrashlyticsTypes.Module;
+  private instace?: import('@react-native-firebase/crashlytics').FirebaseCrashlyticsTypes.Module;
 
   constructor(config: SetupPropsType) {
     super(config);
-    this.instace = crashlytics();
+    try {
+      if (firebaseApp.app()) {
+        const crashlytics = require('@react-native-firebase/crashlytics')();
+        this.instace = crashlytics();
+      }
+    } catch (err) {
+      console.warn(err);
+    }
   }
 
   log(...args: any[]): void {
-    this.instace.log(args.map(String).join(', '));
+    this.instace?.log(args.map(String).join(', '));
   }
 
   warn(...args: any[]): void {
-    this.instace.log('WARN: ' + args.map(String).join(', '));
+    this.instace?.log('WARN: ' + args.map(String).join(', '));
   }
 
   error(error: Error): void {
-    this.instace.recordError(error);
+    this.instace?.recordError(error);
   }
 }
 
